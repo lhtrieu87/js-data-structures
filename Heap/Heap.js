feae819aba9195f437f69e89d165f8f1a68fe9ee
lhtrieu87 <lhtrieu87@gmail.com>
2015-03-07 18:24:11 +0800

    Heap sort

--- Heap/Heap.js
+++ Heap/Heap.js
@@ -1,75 +1,75 @@
-// Insert: O(logN)
-// Pop: O(logN)
-// Build from another array: O(N)
+// Insert:                            O(logN) || O(1)
+// Pop:                               O(logN)
+// Peek:                              O(1)
+// Build from another array:          O(N)
+// Heap.sort (in placed, not stable): O(NlogN)
+
+function Node(key, value) {
+  this.key = key;
+  this.value = value;
+
+  this.toString = function () {
+    return '(' + this.key + ', ' + this.value + ')';
+  }
+}
+
+function swap(array, size, i, j) {
+  if(i >= 0 && j >= 0 && i < size && j < size && i != j) {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+function leftChild(i, size) {
+  if(i < 0) return -1;
+  var l = i * 2 + 1;
+  return l >= size ? -1 : l;
+}
+
+function rightChild(i, size) {
+  if(i < 0) return -1;
+  var r = i * 2 + 2;
+  return r >= size ? -1 : r;
+}
+
+function parent(i, size) {
+  if(i >= size || i <= 0) return -1;
+  return Math.floor((i - 1) / 2);
+}
+
+function bubbleDown(array, size, i) {
+  if(i >= 0 && i < size) {
+    var j = i;
+
+    var l = leftChild(i, size);
+    if(l >= 0 && array[l].key > array[j].key) {
+      j = l;
+    }
+
+    var r = rightChild(i, size);
+    if(r >= 0 && array[r].key > array[j].key) {
+      j = r;
+    }
+
+    if(j != i) {
+      swap(array, size, i, j);
+      bubbleDown(array, size, j);
+    }
+  }
+}
+
+function bubbleUp(array, size, i) {
+  var p = parent(i, size);
+  if(p >= 0 && array[p].key < array[i].key) {
+    swap(array, size, p, i);
+    bubbleUp(array, size, p);
+  }
+}
 
 function Heap() {
-  function Node(key, value) {
-    this.key = key;
-    this.value = value;
-
-    this.toString = function () {
-      return '(' + this.key + ', ' + this.value + ')';
-    }
-  }
-
-  Heap.Node = Node;
-
   var array = [];
   var size = 0;
-
-  function leftChild(i) {
-    if(i < 0) return -1;
-    var l = i * 2 + 1;
-    return l >= size ? -1 : l;
-  }
-
-  function rightChild(i) {
-    if(i < 0) return -1;
-    var r = i * 2 + 2;
-    return r >= size ? -1 : r;
-  }
-
-  function parent(i) {
-    if(i >= size || i <= 0) return -1;
-    return Math.floor((i - 1) / 2);
-  }
-
-  function swap(i, j) {
-    if(i >= 0 && j >= 0 && i < size && j < size && i != j) {
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-  }
-
-  function bubbleUp(i) {
-    var p = parent(i);
-    if(p >= 0 && array[p].key < array[i].key) {
-      swap(p, i);
-      bubbleUp(p);
-    }
-  }
-
-  function bubbleDown(i) {
-    if(i >= 0 && i < size) {
-      var j = i;
-
-      var l = leftChild(i);
-      if(l >= 0 && array[l].key > array[j].key) {
-        j = l;
-      }
-
-      var r = rightChild(i);
-      if(r >= 0 && array[r].key > array[j].key) {
-        j = r;
-      }
-
-      if(j != i) {
-        swap(i, j);
-        bubbleDown(j);
-      }
-    }
-  }
 
   this.clear = function () {
     size = 0;
@@ -87,7 +87,7 @@ function Heap() {
   this.insert = function (key, value) {
     array[size] = new Node(key, value);
     size++;
-    bubbleUp(size - 1);
+    bubbleUp(array, size, size - 1);
   }
 
   this.pop = function () {
@@ -95,9 +95,9 @@ function Heap() {
 
     var max = array[0];
 
-    swap(0, size - 1);
+    swap(array, size, 0, size - 1);
     size--;
-    bubbleDown(0);
+    bubbleDown(array, size, 0);
 
     return max;
   }
@@ -112,8 +112,8 @@ function Heap() {
     array = seedArray;
     size = seedArray.length;
 
-    for(var k = Math.floor((size - 1) / 2); k >= 0; k--) {
-      bubbleDown(k);
+    for(var k = parent(size - 1); k >= 0; k--) {
+      bubbleDown(array, size, k);
     }
   }
 
@@ -126,5 +126,23 @@ function Heap() {
     return strs.join(', ');
   }
 }
+
+Heap.Node = Node;
+Heap.sort = function (initialArray) {
+  var heap = new Heap();
+  heap.build(initialArray);
+  (function heapSort(heap, count) {
+    if(count > 0) {
+      swap(initialArray, count, 0, count - 1);
+      bubbleDown(initialArray, count - 1, 0);
+
+      heapSort(heap, count - 1);
+    }
+  }(heap, initialArray.length));
+
+  initialArray.reverse();
+
+  return initialArray;
+};
 
 module.exports = Heap;
--- Heap/test.js
+++ Heap/test.js
@@ -9,7 +9,7 @@ describe('Heap', function () {
 
   it('build', function () {
     var Node = Heap.Node;
-    
+
     heap.build([]);
     heap.getSize().should.equal(0);
     heap.isEmpty().should.equal(true);
@@ -45,6 +45,10 @@ describe('Heap', function () {
     heap.insert(5, 5);
     heap.getSize().should.equal(4);
     heap.toString().should.equal('(5, 5), (3, 3), (2, 2), (1, 1)');
+
+    heap.insert(9, 9);
+    heap.getSize().should.equal(5);
+    heap.toString().should.equal('(9, 9), (5, 5), (2, 2), (1, 1), (3, 3)');
   });
 
   it('pop', function () {
@@ -58,10 +62,12 @@ describe('Heap', function () {
     max = heap.pop();
     max.key.should.equal(16);
     max.value.should.equal(16);
+    heap.getSize().should.equal(9);
 
     max = heap.pop();
     max.key.should.equal(14);
     max.value.should.equal(14);
+    heap.getSize().should.equal(8);
   });
 
   it('peek', function () {
@@ -75,5 +81,26 @@ describe('Heap', function () {
     max = heap.peek();
     max.key.should.equal(16);
     max.value.should.equal(16);
+    heap.getSize().should.equal(10);
+  });
+
+  it('heap sort', function () {
+    var Node = Heap.Node;
+    var initialList = [new Node(4, 4), new Node(1, 1), new Node(3, 3), new Node(2, 2), new Node(16, 16), new Node(9, 9), new Node(10, 10), new Node(14, 14), new Node(8, 8), new Node(7, 7)];
+    var sortedList = Heap.sort(initialList);
+
+    // In-place sort.
+    sortedList.should.equal(initialList);
+
+    sortedList[0].key.should.equal(16);
+    sortedList[1].key.should.equal(14);
+    sortedList[2].key.should.equal(10);
+    sortedList[3].key.should.equal(9);
+    sortedList[4].key.should.equal(8);
+    sortedList[5].key.should.equal(7);
+    sortedList[6].key.should.equal(4);
+    sortedList[7].key.should.equal(3);
+    sortedList[8].key.should.equal(2);
+    sortedList[9].key.should.equal(1);
   });
 })
